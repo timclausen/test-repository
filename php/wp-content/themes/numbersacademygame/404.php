4f5cd12952635a5c21d3019a342ad6dea1a9055e
Tim Clausen <tc@konstellation.dk>
2012-12-27 21:08:15 +0100

    added intitial wordpress installation

---
+++ php/wp-content/themes/numbersacademygame/404.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+<?php get_footer(); ?>
